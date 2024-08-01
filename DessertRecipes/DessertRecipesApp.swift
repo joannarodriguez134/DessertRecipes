4136a264dc2c917551e0c0d68cb9bf2c94b41f11
Joanna Rodriguez <joannarodriguez134@gmail.com>
2024-08-01 16:56:36 -0500

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by Joanna Rodriguez on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
