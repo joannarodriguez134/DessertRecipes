710c4633f2f3bf359b91bf368f663ff56fda9fc9
Joanna Rodriguez <joannarodriguez134@gmail.com>
2024-08-02 15:09:30 -0500

    Fetching measurements

--- DessertRecipes/RecipeDetailsView.swift
+++ DessertRecipes/RecipeDetailsView.swift
@@ -11,7 +11,30 @@ struct RecipeDetailsView: View {
     let desserts: Meals
     @State private var recipes: Recipes?
     @EnvironmentObject var dessertService: DessertService
+    let keyPaths: [KeyPath<Recipes, String?>] = [
 
+        \Recipes.strMeasure1,
+         \Recipes.strMeasure2,
+         \Recipes.strMeasure3,
+         \Recipes.strMeasure4,
+         \Recipes.strMeasure5,
+         \Recipes.strMeasure6,
+         \Recipes.strMeasure7,
+         \Recipes.strMeasure8,
+         \Recipes.strMeasure9,
+         \Recipes.strMeasure10,
+         \Recipes.strMeasure11,
+         \Recipes.strMeasure12,
+         \Recipes.strMeasure13,
+         \Recipes.strMeasure14,
+         \Recipes.strMeasure15,
+         \Recipes.strMeasure16,
+         \Recipes.strMeasure17,
+         \Recipes.strMeasure18,
+         \Recipes.strMeasure19,
+         \Recipes.strMeasure20
+        ]
+    
     var body: some View {
         ScrollView {
             if let recipes = recipes {
@@ -26,6 +49,14 @@ struct RecipeDetailsView: View {
 
                     Text(recipes.strInstructions)
                         .font(.caption)
+                    
+
+                        ForEach(0..<keyPaths.count, id: \.self) { index in
+                            if let strMeasure =  recipes[keyPath: keyPaths[index]], !strMeasure.isEmpty {
+                                Text(strMeasure)
+                            }
+                        }
+                    
                 }
                 .multilineTextAlignment(.leading)
                 .padding(.horizontal, 5)
